99d6c6150b24712175a09337573777baef2cabdc
kier-ious <kier.diemer@gmail.com>
2024-09-20 01:45:33 -0500

    ended up adding the persistConfig to my store

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,22 @@
 import { Header } from "./Header";
-import  Footer from "./Footer";
+import Footer from "./Footer";
 import { Board } from "./Board";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "../store";
 import store from "../store";
-
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="">
-        <Header />
-        <Board />
-        <Footer />
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="">
+          <Header />
+          <Board />
+          <Footer />
+        </div>
+      </PersistGate>
     </Provider>
-
   );
 }
 
--- src/store.ts
+++ src/store.ts
@@ -2,20 +2,34 @@ import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import cardsReducer from "../src/components/slices/cardsSlice";
 import listsReducer from "../src/components/slices/listsSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
 
 
-const store = configureStore({
-  reducer: {
-    lists: listsReducer,
-    cards: cardsReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  lists: listsReducer,
+  cards: cardsReducer,
 });
 
-// These types are helpful for the typescript autocomplete
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+// These types are helpful for TypeScript autocomplete
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-//Add types to dispatch and selector hooks
+// Add types to dispatch and selector hooks
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
