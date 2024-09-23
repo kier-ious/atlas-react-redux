8fc6a438ed8e1c94b01688692afdf98bdb8551e4
kier-ious <kier.diemer@gmail.com>
2024-09-22 22:04:04 -0500

    started lists test, it's not complete. reviewing unit testing live code video.

---
+++ src/__tests__/listsSlice.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest';
+import reducer, { addList, deleteList, clearBoard, deleteCard, addCard } from '../components/slices/listsSlice';
+
+
+describe('listsSlice', () => {
+  const initialState = {
+    lists: [],
+    cards: {},
+  };
+
+  it('Should handle addList', () => {
+    const action
+  })
+})
