6850c14ba2eaf154f6dd08e345df45b68275013d
kier-ious <kier.diemer@gmail.com>
2024-09-23 14:18:51 -0500

    Got all cardsSlice tests to pass! Originally I was trying to mock the entire reduxjs toolkit, but instead I used asynchronous import handling wrapping the mock function in a async callback. also checks if import mod is in fact a object.

--- src/__tests__/cardsSlice.test.ts
+++ src/__tests__/cardsSlice.test.ts
@@ -1,7 +1,17 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import cardsReducer, { addCard, deleteCard, clearBoard } from '../components/slices/cardsSlice';
+// import { nanoid } from '@reduxjs/toolkit';
 
-
+vi.mock('@reduxjs/toolkit', async (importOriginal) => {
+  const actual = await importOriginal();
+  if (typeof actual === 'object' && actual !== null) {
+    return {
+      ...actual,
+      nanoid: () => '1',
+    };
+  }
+  throw new Error('Expected @reduxjs/toolkit to import an object');
+});
 
 describe('cardsSlice', () => {
   const card = {
@@ -11,10 +21,14 @@ describe('cardsSlice', () => {
     description: '',
     text: ''
   };
+
   it('Should allow user to add card (addCard)', () => {
     const initialState = { cards: {} };
     const userAction = addCard({
-      listId: 'list-001', title: 'Card Title', text: '', description: '',
+      listId: 'list-001',
+      title: 'Card Title',
+      text: '',
+      description: ''
     });
     const newState = cardsReducer(initialState, userAction);
       expect(newState.cards[card.id]).toEqual(card);
@@ -22,10 +36,10 @@ describe('cardsSlice', () => {
 
 
 
-  it('Should delete card selected card (deleteCard)', () => {
+  it('Should delete selected card (deleteCard)', () => {
     const initialState = {
       cards: { '1': { id: '1', title: 'Card Title', listId: 'list-001', description: '', text: '' } },
-    }
+    };
     const userAction = deleteCard('1');
     const newState = cardsReducer(initialState, userAction);
       expect(newState.cards['1']).toBeUndefined();
